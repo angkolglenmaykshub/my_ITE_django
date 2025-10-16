717570d4aea326b1054a8a523ee6d5dd2777bf81
Myname g <glenemiko.jaudian@msunaawan.edu.ph>
2025-10-16 20:31:57 +0800

    creating cbv for students

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from api.views import function_based_view
+from api.views import student_function_based_view
+from api.views import employee_function_based_view
+from api.views import student_class_based_view
 
 urlpatterns = [
-    path('fbv-students/', function_based_view.studentView)
+    path('fbv-students/', student_function_based_view.studentView),
+    path('fbv-students/<int:student_id>/', student_function_based_view.student),
+    path('fbv-employees/', employee_function_based_view.employeeView),
+    path('fbv-employees/<int:employee_id>/', employee_function_based_view.employee),
+
+    #cbv part
+    path('cbv-students/', student_class_based_view.Student.as_view()),
 ]
+        
