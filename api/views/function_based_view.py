d212eda59c0976d421aad70a19f4d8358bd59d90
Artchel <artchel.lapuz@msunaawan.edu.ph>
2025-10-16 15:01:23 +0800

    put and delete

--- api/views/function_based_view.py
+++ api/views/function_based_view.py
@@ -1,21 +1,45 @@
-from django.http import HttpResponse
-from students.models import Students
-from api.serializers import StudentSerializer  
-from rest_framework.decorators import api_view
-from rest_framework import status
+from django.shortcuts import render, get_object_or_404  
+from students.models import Student
+from api.serializer import StudentSerializer
 from rest_framework.response import Response
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-@api_view(['GET', 'POST'])
-def studentView(request):
-    if request.method == "GET":
-        students = Students.objects.all()
-        serializer = StudentSerializer(students, many=True)
-        print(students, serializer.data)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    if request.method == "POST":
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Create your views here.
+
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def studentView(request, pk=None):
+    if pk is None:
+        if(request.method == 'GET'):
+           # Get all the data from the student table
+           students = Student.objects.all()
+           serializer = StudentSerializer(students, many=True)
+           print (students, serializer.data)
+           return Response(serializer.data, status=status.HTTP_200_OK)
+
+        if(request.method == 'POST'):
+            serializer = StudentSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            print(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        student = get_object_or_404(Student, pk=pk)
+     #specific student data
+        if request.method == 'GET':   
+            serializer = StudentSerializer(student)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        elif request.method == 'PUT':
+            serializer = StudentSerializer(student, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        elif request.method == 'DELETE':
+            student.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
